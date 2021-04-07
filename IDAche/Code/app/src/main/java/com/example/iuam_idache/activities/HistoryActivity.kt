7774b71e5eb7f2dd578f88fb15b5fab1ea3c2417
isaia.spinelli <isaia.spinelli@heig-vd.ch>
2021-04-07 17:12:06 +0200

    Add feature for get all event + prepare faeture for add new event

--- IDAche/Code/app/src/main/java/com/example/iuam_idache/activities/HistoryActivity.kt
+++ IDAche/Code/app/src/main/java/com/example/iuam_idache/activities/HistoryActivity.kt
@@ -2,15 +2,21 @@ package com.example.iuam_idache.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageButton
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.iuam_idache.R
 import com.example.iuam_idache.adapters.WPDayPickerAdapter
+import com.example.iuam_idache.apiREST.classes.ClientRestAPI
+import com.example.iuam_idache.apiREST.interfaces.getMyEventsCallback
+import com.example.iuam_idache.apiREST.models.EventsAche
 import com.super_rabbit.wheel_picker.OnValueChangeListener
 import com.super_rabbit.wheel_picker.WheelPicker
 
+// TODO -> bug when extremity item are selected
 class HistoryActivity : AppCompatActivity() {
+
+    private lateinit var eventsDisplay : List<EventsAche>
 
     //-------------- Buttons
     private lateinit var searchButton : ImageButton
@@ -19,25 +25,26 @@ class HistoryActivity : AppCompatActivity() {
     //-------------- DatePicker
     private lateinit var datePicker : WheelPicker
     private val nbDataToDisplay : Int = 5
-    private val dates : Array<String> = arrayOf(
+    private val dates : MutableList<String> = mutableListOf(
         "01/01/2021",
         "02/01/2021",
         "03/01/2021",
         "04/01/2021",
         "05/01/2021",
-        "06/01/2021",
-        "07/01/2021",
-        "08/01/2021",
-        "09/01/2021",
-        "10/01/2021",
-        "11/01/2021",
-        "12/01/2021",
-        "13/01/2021",
+//        "06/01/2021",
+//        "07/01/2021",
+//        "08/01/2021",
+//        "09/01/2021",
+//        "10/01/2021",
+//        "11/01/2021",
+//        "12/01/2021",
+//        "13/01/2021",
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history)
+
 
         //------------------------------- Buttons --------------------------------
         // Back button
@@ -51,9 +58,54 @@ class HistoryActivity : AppCompatActivity() {
         searchButton = findViewById(R.id.history_toolbar_search_imagebutton)
         searchButton.setOnClickListener {
             // TODO -> make a research in the database
+
+            // TODO get my real ID
+            // Get all my events
+            val myUserID = 3
+
+            val myClientRestAPI = ClientRestAPI()
+            myClientRestAPI.getMyEvents(myUserID, object : getMyEventsCallback {
+                override fun onSuccess(myEvents: List<EventsAche?>?) {
+                    // TODO : add notif
+                    Log.v(
+                        "TAG",
+                        "getMyEvents -> Sucess (" + myEvents!!.size + ") ! : " + myEvents.toString()
+                    )
+
+                    // remove old dates
+                    dates.removeAll { true }
+                    // take real date events
+                    myEvents.forEach { dates.add(it?.event_timestamp.toString().substring(0,10)) }
+
+                    // update list
+                    updateList()
+                    eventsDisplay = myEvents as List<EventsAche>;
+
+                }
+
+                override fun onFailure() {
+                    // TODO : add notif
+                    Log.v("TAG", "getMyEvents -> Failed ! ")
+                }
+            })
         }
 
         //----------------------------- Number picker -----------------------------
+        updateList()
+
+        // OnValueChangeListener
+        datePicker.setOnValueChangeListener(object : OnValueChangeListener {
+            override fun onValueChange(picker: WheelPicker, oldVal: String, newVal: String) {
+                // TODO -> display informations in the recap views
+                // https://stackoverflow.com/questions/33053765/how-to-make-a-wheel-picker/39662187
+                Log.v("TAG", "onValueChange old : " + oldVal + "  new : " + newVal)
+            }
+        })
+
+
+    }
+
+    private fun updateList() {
         datePicker = findViewById(R.id.activity_history_datePicker)
         // Date picker
         // Set rounded wrap enable
@@ -61,7 +113,7 @@ class HistoryActivity : AppCompatActivity() {
         // Set wheel item count
         datePicker.setWheelItemCount(nbDataToDisplay)
         // Set wheel max index
-        datePicker.setMax(dates.size-1)
+        datePicker.setMax(dates.size - 1)
         // Set wheel min index
         datePicker.setMin(0)
         // Set selected text color
@@ -69,16 +121,7 @@ class HistoryActivity : AppCompatActivity() {
         // Set unselected text color
         datePicker.setUnselectedTextColor(R.color.colorGreenPrimaryDark)
         // Set user defined adapter
-        datePicker.setAdapter(WPDayPickerAdapter(dates, nbDataToDisplay))
-
-        // OnValueChangeListener
-        datePicker.setOnValueChangeListener(object : OnValueChangeListener {
-            override fun onValueChange(picker: WheelPicker, oldVal: String, newVal: String) {
-                // TODO -> display informations in the recap views
-            }
-        })
-
-
+        datePicker.setAdapter(WPDayPickerAdapter(dates.toTypedArray(), nbDataToDisplay))
     }
 
 }
