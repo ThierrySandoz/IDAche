6a0225c986b446339fa5e0e09832753a5f338b29
isaia.spinelli <isaia.spinelli@heig-vd.ch>
2021-04-30 10:47:40 +0200

    Display Location + code weather in String + wind speed

--- IDAche/Code/app/src/main/java/com/example/iuam_idache/apiREST/models/EventsAche.kt
+++ IDAche/Code/app/src/main/java/com/example/iuam_idache/apiREST/models/EventsAche.kt
@@ -2,19 +2,20 @@ package com.example.iuam_idache.apiREST.models
 
 import java.sql.Timestamp
 
-// TODO code weather -> String ou byte ?
 class EventsAche {
     var event_id: Long = -1
     var event_timestamp: Timestamp? = null
     var event_geoloc_lat = -1f
     var event_geoloc_lon = -1f
+    var event_localisation = ""
     var event_temp = -1
     var user_id: Long = -1
     var event_humidity = -1
     var event_visibility = -1
+    var event_wind_speed = -1
 
     //String event_code_weather = null;
-    var event_code_weather: Byte = -1
+    var event_code_weather: String = ""
     var event_pressure = -1
     var event_HR_ave = -1
     var event_HR_max = -1
@@ -29,11 +30,13 @@ class EventsAche {
     constructor(
         event_geoloc_lat: Float,
         event_geoloc_lon: Float,
+        event_localisation: String,
         event_temp: Int,
         user_id: Long,
         event_humidity: Int,
         event_visibility: Int,
-        event_code_weather: Int,
+        event_wind_speed: Int,
+        event_code_weather: String,
         event_pressure: Int,
         event_hr_ave: Int,
         event_hr_max: Int,
@@ -46,11 +49,13 @@ class EventsAche {
     ) {
         this.event_geoloc_lat = event_geoloc_lat
         this.event_geoloc_lon = event_geoloc_lon
+        this.event_localisation = event_localisation
         this.event_temp = event_temp
         this.user_id = user_id
         this.event_humidity = event_humidity
         this.event_visibility = event_visibility
-        this.event_code_weather = event_code_weather.toByte()
+        this.event_wind_speed = event_wind_speed
+        this.event_code_weather = event_code_weather
         this.event_pressure = event_pressure
         event_HR_ave = event_hr_ave
         event_HR_max = event_hr_max
@@ -72,11 +77,13 @@ class EventsAche {
     ) : this(
         -1f,
         -1f,
+        "",
         -1,
         user_id,
         -1,
         -1,
         -1,
+        "",
         -1,
         -1,
         -1,
@@ -106,11 +113,13 @@ class EventsAche {
     ) : this(
         -1f,
         -1f,
+        "",
         -1,
         user_id,
         -1,
         -1,
         -1,
+        "",
         -1,
         event_hr_ave,
         event_hr_max,
@@ -127,11 +136,13 @@ class EventsAche {
     constructor(
         event_geoloc_lat: Float,
         event_geoloc_lon: Float,
+        event_localisation: String,
         event_temp: Int,
         user_id: Long,
         event_humidity: Int,
         event_visibility: Int,
-        event_code_weather: Int,
+        event_wind_speed: Int,
+        event_code_weather: String,
         event_pressure: Int,
         event_ache_locality: Int,
         event_ache_power: Int,
@@ -139,10 +150,12 @@ class EventsAche {
     ) : this(
         event_geoloc_lat,
         event_geoloc_lon,
+        event_localisation,
         event_temp,
         user_id,
         event_humidity,
         event_visibility,
+        event_wind_speed,
         event_code_weather,
         event_pressure,
         -1,
@@ -160,11 +173,13 @@ class EventsAche {
     constructor(user_id: Long) : this(
         -1f,
         -1f,
+        "",
         -1,
         user_id,
         -1,
         -1,
         -1,
+        "",
         -1,
         -1,
         -1,
@@ -178,7 +193,7 @@ class EventsAche {
     }
 
     override fun toString(): String {
-        return """ID: $event_id timestamp: $event_timestamp event_geoloc_lat: $event_geoloc_lat event_geoloc_lon: $event_geoloc_lon event_temp: $event_temp user_id: $user_id event_humidity: $event_humidity event_visibility: $event_visibility event_code_weather: $event_code_weather event_pressure: $event_pressure event_HR_ave: $event_HR_ave event_HR_max: $event_HR_max event_HR_min: $event_HR_min event_ACC_activity: $event_ACC_activity event_ACC_sleep: $event_ACC_sleep event_Ache_locality: $event_Ache_locality event_Ache_power: $event_Ache_power event_Ache_power: $event_Ache_power event_Ache_type: $event_Ache_type
+        return """ID: $event_id timestamp: $event_timestamp event_geoloc_lat: $event_geoloc_lat event_geoloc_lon: $event_geoloc_lon event_localisation: $event_localisation event_temp: $event_temp user_id: $user_id event_humidity: $event_humidity event_visibility: $event_visibility event_wind_speed $event_wind_speed event_code_weather: $event_code_weather event_pressure: $event_pressure event_HR_ave: $event_HR_ave event_HR_max: $event_HR_max event_HR_min: $event_HR_min event_ACC_activity: $event_ACC_activity event_ACC_sleep: $event_ACC_sleep event_Ache_locality: $event_Ache_locality event_Ache_power: $event_Ache_power event_Ache_power: $event_Ache_power event_Ache_type: $event_Ache_type
 """
     }
 }
